da8c13f45de6e77e4bfca44ba25c1081fb6ca657
GitThaRun <tharunjami5@gmail.com>
2026-02-28 12:32:11 +0530

    Added Java file

---
+++ Java Arrays/Buses_Required.java
@@ -0,0 +1,45 @@
+// Problem Statement : In a Bus stop queue there are 'N' groups of people in it.In every group there are a[i] people in it.Every 30min an empty bus arrives at the bustop and it can carry atmost 'M' people in it.
+// Note that the order of queue never changes 
+// Yout task is to count the number of buses required to transport all the groups to Jeju Island.
+
+// Input :
+// 4
+// 3
+// 2 3 2 1
+// Output : 
+// 3
+
+import java.util.Scanner;
+public class Buses_Required {
+    
+    public static int countBuses(int arr[],int M){
+
+        int count = 1;
+        int load = 0;
+
+        for(int i = 0;i < arr.length;i++){
+            if(load + arr[i] <= M){
+                load += arr[i];
+            }
+            else{
+                count++;
+                load = arr[i];
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int people[] = new int[N];
+        for(int i = 0;i < N;i++){
+            people[i] = sc.nextInt();
+        }
+
+        System.out.println(countBuses(people,M));
+        sc.close();
+    }
+}
