e632f5d6f31b9f8a4aa3ad56952c6a6f93f511d2
GitThaRun <tharunjami5@gmail.com>
2026-07-18 20:38:21 +0530

    Added Lower_Bound

---
+++ 03_BinarySearch/Lower_Bound.java
@@ -0,0 +1,57 @@
+// The lower bound is the smallest index, ind, where arr[ind] >= x. But if any such index is not found, the lower bound algorithm returns n i.e. size of the given array.
+// Example 1:
+// Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+// Result: 1
+// Explanation: Index 1 is the smallest index such that arr[1] >= x.
+
+// Example 2:
+// Input Format: N = 5, arr[] = {3,5,8,15,19}, x = 9
+// Result: 3
+// Explanation: Index 3 is the smallest index such that arr[3] >= x.
+
+// Platform : Striver's Sheet
+// Level : Easy
+
+// Approaches:
+// 1)BruteForce : Time complexity : O(N), Space Complexity : O(1)
+// 2)Optimal : Time Complexity : O(log N), Space complexity : O(1)
+import java.util.Scanner;
+public class Lower_Bound {
+    public static int BruteLowerBound(int[] nums, int x) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= x) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static int OptimalLowerBound(int[] nums, int x) {
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(BruteLowerBound(arr, x));
+        // System.out.println(OptimalLowerBound(arr, x));
+        sc.close();
+    }
+}
