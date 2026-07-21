32b063b9410855b8fce4a727cef0de9f22d05044
GitThaRun <tharunjami5@gmail.com>
2026-07-21 22:37:46 +0530

    Added Isomorphic_String

---
+++ 02_Strings/Isomorphic_String.java
@@ -0,0 +1,38 @@
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+// Example 1:
+
+// Input: s = "egg", t = "add"
+
+// Output: true
+
+// Explanation:
+
+// The strings s and t can be made identical by:
+
+// Mapping 'e' to 'a'.
+// Mapping 'g' to 'd'.
+
+public class Isomorphic_String {
+    public static boolean isIsomorphic(String s, String t) {
+        int[] m1 = new int[256], m2 = new int[256];
+
+        for(int i = 0;i < s.length();i++){
+
+            if(m1[s.charAt(i)] != m2[t.charAt(i)]){
+                return false;
+            }
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+
+        System.out.println(isIsomorphic(s, t));
+    }
+}
