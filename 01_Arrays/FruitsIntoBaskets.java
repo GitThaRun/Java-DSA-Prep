cd9cd1fe820170137c415e742d4e24774cf0be43
GitThaRun <tharunjami5@gmail.com>
2026-08-03 19:15:07 +0530

    Added Leetcode 904

---
+++ 01_Arrays/FruitsIntoBaskets.java
@@ -0,0 +1,47 @@
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+
+// Example 1:
+
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+
+// Platform: Leetcode
+// Level: Medium
+// Time Complexity: O(n), Space Complexity: O(1)
+
+import java.util.HashMap;
+
+public class FruitsIntoBaskets {
+    public static int totalFruit(int[] fruits) {
+        int left = 0, ans = 0;
+        HashMap<Integer,Integer> freq = new HashMap<>();
+
+        for(int right = 0;right < fruits.length;right++){
+
+            freq.put(fruits[right],freq.getOrDefault(fruits[right],0) + 1);
+
+            while(freq.size() > 2){
+                freq.put(fruits[left],freq.get(fruits[left]) - 1);
+
+                if(freq.get(fruits[left]) == 0){
+                    freq.remove(fruits[left]);
+                }
+                left++;
+            }
+            ans = Math.max(ans,right - left + 1);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] fruits = {1,2,1};
+        System.out.println(totalFruit(fruits)); 
+    }
+}
